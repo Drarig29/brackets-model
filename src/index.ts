e1a058d1c70b50068854aa2bba19ce5d392b0236
Corentin Girard <corentingirard.dev@gmail.com>
2020-06-03 16:38:35 +0200

    Finally working...

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-import { MatchScores, OrderingType } from "../types";
-
 /**
  * Makes pairs with each element and its next one.
  * @example [1, 2, 3, 4] --> [[1, 2], [3, 4]]
  */
-export function makePairs(array: any[]): any[][];
+function makePairs(array: any[]): any[][];
 
 /**
  * Makes pairs with one element from `left` and the other from `right`.
  * @example [1, 2] + [3, 4] --> [[1, 3], [2, 4]]
  */
-export function makePairs(left: any[], right: any[]): any[][];
+function makePairs(left: any[], right: any[]): any[][];
 
-export function makePairs(left: any[], right?: any[]): any[][] {
+function makePairs(left: any[], right?: any[]): any[][] {
     if (!right) {
         ensureEvenSized(left);
         return left.map((current, i) => (i % 2 === 0) ? [current, left[i + 1]] : [])
@@ -23,25 +21,25 @@ export function makePairs(left: any[], right?: any[]): any[][] {
     return left.map((current, i) => [current, right[i]]);
 }
 
-export function ensureEvenSized(array: any[]) {
+function ensureEvenSized(array: any[]) {
     if (array.length % 2 === 1) {
         throw Error('Array size must be even.');
     }
 }
 
-export function ensureEquallySized(left: any[], right: any[]) {
+function ensureEquallySized(left: any[], right: any[]) {
     if (left.length !== right.length) {
         throw Error('Arrays size must be equal.');
     }
 }
 
-export function ensurePowerOfTwoSized(array: any[]) {
+function ensurePowerOfTwoSized(array: any[]) {
     if (!Number.isInteger(Math.log2(array.length))) {
         throw Error('Array size must be a power of 2.');
     }
 }
 
-export function ensureNotTied(scores: MatchScores) {
+function ensureNotTied(scores: MatchScores) {
     if (scores[0] === scores[1]) {
         throw Error(`${scores[0]} and ${scores[1]} are tied. It cannot be.`);
     }
@@ -49,7 +47,7 @@ export function ensureNotTied(scores: MatchScores) {
 
 // https://web.archive.org/web/20200601102344/https://tl.net/forum/sc2-tournaments/202139-superior-double-elimination-losers-bracket-seeding
 
-export const ordering = {
+const ordering = {
     natural: (array: any[]) => [...array],
     reverse: (array: any[]) => array.reverse(),
     half_shift: (array: any[]) => [...array.slice(array.length / 2), ...array.slice(0, array.length / 2)],
@@ -61,7 +59,7 @@ export const ordering = {
     },
 }
 
-export const defaultMinorOrdering: { [key: number]: OrderingType[] } = {
+const defaultMinorOrdering: { [key: number]: OrderingType[] } = {
     8: ['natural', 'reverse', 'natural'],
     16: ['natural', 'reverse_half_shift', 'reverse', 'natural'],
     32: ['natural', 'reverse', 'half_shift', 'natural', 'natural'],
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,9 +1,9 @@
-export type TournamentType = "pools" | "single_elimination" | "double_elimination";
-export type OrderingType = "natural" | "reverse" | "half_shift" | "reverse_half_shift" | "pair_flip";
+type TournamentType = "pools" | "single_elimination" | "double_elimination";
+type OrderingType = "natural" | "reverse" | "half_shift" | "reverse_half_shift" | "pair_flip";
 
-export type Teams = string[];
+type Teams = string[];
 
-export type MatchScores = [number, number];
-export type RoundScores = MatchScores[];
-export type BracketScores = RoundScores[];
-export type TournamentResults = BracketScores[];
+type MatchScores = [number, number];
+type RoundScores = MatchScores[];
+type BracketScores = RoundScores[];
+type TournamentResults = BracketScores[];
