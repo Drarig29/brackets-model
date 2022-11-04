6608d18986ddfe1adef770acdcf7f24ba39a1852
Corentin Girard <corentin.girard@datadoghq.com>
2022-11-04 04:33:37 +0100

    Support custom participants (#7)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { GrandFinalType, Result, RoundRobinMode, SeedOrdering, StageType } from './unions';
-import { InputStage, Seeding, StageSettings } from './input';
+import { InputStage, Seeding, CustomParticipant, StageSettings } from './input';
 import { Participant, Stage, Group, Round, Match, MatchGame } from './storage';
 import { MatchResults, ParticipantResult, Status } from './other';
 
 export {
     GrandFinalType, Result, RoundRobinMode, SeedOrdering, StageType,
-    InputStage, Seeding, StageSettings,
+    InputStage, Seeding, CustomParticipant, StageSettings,
     Participant, Stage, Group, Round, Match, MatchGame,
     MatchResults, ParticipantResult, Status,
 };
