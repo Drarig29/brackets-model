24c37e69012992d1b331a48dd481fd3a1ee98f23
corentin <corentin.girard@datadoghq.com>
2023-04-09 12:33:50 +0200

    Fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-import { GrandFinalType, Result, RoundRobinMode, SeedOrdering, StageType } from './unions';
-import { InputStage, Seeding, CustomParticipant, StageSettings } from './input';
-import { Participant, Stage, Group, Round, Match, MatchGame } from './storage';
-import { MatchResults, ParticipantResult, Status } from './other';
-
-export {
-    GrandFinalType, Result, RoundRobinMode, SeedOrdering, StageType,
-    InputStage, Seeding, CustomParticipant, StageSettings,
-    Participant, Stage, Group, Round, Match, MatchGame,
-    MatchResults, ParticipantResult, Status,
-};
+export * from './unions';
+export * from './input';
+export * from './storage';
+export * from './other';
